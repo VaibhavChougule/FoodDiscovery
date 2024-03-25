83a0167dc996be7979e552e795c14fe088591296
Vaibhav <vaibhavchougule475@gmail.com>
2024-03-25 05:31:42 +0530

    updated proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
  server:{
   proxy:{
-    '/api':'http://127.0.0.1:3009'
+    '/api':'https://food-discovery-server.vercel.app'
   }
  }
 })
