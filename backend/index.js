c3e02a123b60f6464ace1cf7c954b2c75e82e92d
Vaibhav <vaibhavchougule475@gmail.com>
2024-03-25 16:03:49 +0530

    changes while deploying

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ const ipWhitelistMiddleware = (req, res, next) => {
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
-app.use('/' , getResto)
+app.use('/api/getResto' , getResto)
 app.use('/' , homeRoute);
 
 app.use('/' , registerRestoRoute);
